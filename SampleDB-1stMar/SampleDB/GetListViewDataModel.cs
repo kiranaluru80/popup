65aefaeafb4dcd6001ddc0813f9d9057989e89a3
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2018-03-09 07:18:34 -0500

    popup

---
+++ SampleDB-1stMar/SampleDB/GetListViewDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SampleDB
+{
+        public class GetListViewDataModel
+        {
+            public int Request_ID { get; set; }
+            public string Requested_By { get; set; }
+            public string Description { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public Group Group { get; set; }
+            public object MajorGroup_Id { get; set; }
+            public User User { get; set; }
+            public object Org_Id { get; set; }
+            public object Active { get; set; }
+        }
+    }
+
---
+++ SampleDB-1stMar/SampleDB/UserGroupDetails.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SampleDB
+{
+    public partial class UserGroupDetails : ContentPage
+    {
+        public UserGroupDetails(GetListViewDataModel userObj)
+        {
+            InitializeComponent();
+            gridRef.BindingContext = userObj;
+
+            grantRef.Clicked += (sender, e) => {
+                alertViewRef.IsVisible = true;
+            };
+
+            //alertNoRef.Clicked += (sender, e) => {
+            //    alertViewRef.IsVisible = false;
+            //};
+        }
+    }
+}
